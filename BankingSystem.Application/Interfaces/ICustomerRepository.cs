b28106c33755d27e347140de6c68931deb72011d
PTP <93674251+PianoTutorialsPlus@users.noreply.github.com>
2025-10-10 14:22:20 +0200

    Added command incl handler

---
+++ BankingSystem.Application/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using BankingSystem.Domain.Entities;
+
+namespace BankingSystem.Application.Interfaces;
+
+public interface ICustomerRepository : IRepository<Customer>
+{
+    Task SaveChangesAsync();
+}
