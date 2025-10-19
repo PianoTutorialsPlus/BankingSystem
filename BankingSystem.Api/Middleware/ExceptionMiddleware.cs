014018093c451cb450b463353c8d4a78109c69cb
PTP <93674251+PianoTutorialsPlus@users.noreply.github.com>
2025-10-19 16:24:15 +0200

    Added ExceptionMiddleware

---
+++ BankingSystem.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,68 @@
+﻿using BankingSystem.Api.Models;
+using BankingSystem.Application.Exceptions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace BankingSystem.Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionMiddleware> logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            var statusCode = HttpStatusCode.InternalServerError;
+            CustomProblemDetails problem;
+
+            switch (ex)
+            {
+                case BadRequestException badRequestException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    problem = new CustomProblemDetails
+                    {
+                        Title = badRequestException.Message,
+                        Status = (int)statusCode,
+                        Detail = badRequestException.InnerException?.Message,
+                        Type = nameof(BadRequestException),
+                        Errors = badRequestException.ValidationErrors
+                    };
+                    break;
+                default:
+                    problem = new CustomProblemDetails
+                    {
+                        Title = ex.Message,
+                        Status = (int)statusCode,
+                        Detail = ex.StackTrace,
+                        Type = nameof(HttpStatusCode.InternalServerError),
+                    };
+                    break;
+            }
+
+            httpContext.Response.StatusCode = (int)statusCode;
+            var logMessage = JsonConvert.SerializeObject(problem);
+            logger.LogError(logMessage);
+
+            await httpContext.Response.WriteAsJsonAsync(problem);
+        }
+    }
+}
--- BankingSystem.Api/Program.cs
+++ BankingSystem.Api/Program.cs
@@ -1,9 +1,17 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using BankingSystem.Persistence;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
+using BankingSystem.Api.Middleware;
+using BankingSystem.Application.Contracts;
+using BankingSystem.Application.Contracts.Repository;
+using BankingSystem.Application.Features.Customers.Commands.CreateCustomer;
+using BankingSystem.Persistence.DatabaseContext;
 using BankingSystem.Persistence.Repositories;
 using BankingSystem.Persistence.Services;
+using MediatR.Extensions.Autofac.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection.Builder;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
 // Configuration
-var conn = builder.Configuration.GetConnectionString("Default") ?? "Data Source=banking.db";
+var conn = builder.Configuration.GetConnectionString("Default") ?? "Data Source=db_bankingSystem.db";
 var checksumSecret = builder.Configuration["ChecksumSecret"] ?? "dev-secret";
 
 // Add services to DI
@@ -24,17 +32,29 @@ builder.Services.AddDbContext<BankingDbContext>(opts => opts.UseSqlite(conn));
 
 builder.Host.ConfigureContainer<ContainerBuilder>(container =>
 {
+    container.RegisterAutoMapper(typeof(CreateCustomerCommand).Assembly);
+
+    var configuration = MediatRConfigurationBuilder
+    .Create("", typeof(CreateCustomerCommand).Assembly)
+    .WithAllOpenGenericHandlerTypesRegistered()
+    .Build();
+
+    container.RegisterMediatR(configuration);
+
     // Repositories
-    container.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(BankingSystem.Application.Interfaces.IRepository<>)).InstancePerLifetimeScope();
+    container.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
 
     // App services
-    container.RegisterType<HmacChecksumService>().As<BankingSystem.Application.Interfaces.IChecksumService>().WithParameter("secret", checksumSecret).SingleInstance();
+    container.RegisterType<HmacChecksumService>().As<IChecksumService>().WithParameter("secret", checksumSecret).SingleInstance();
     container.RegisterType<AccountService>().SingleInstance();
 
     // You may register other handlers here...
+    container.RegisterType<CustomerRepository>().As<ICustomerRepository>().SingleInstance();
 });
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Migrate/Ensure DB
 using (var sc = app.Services.CreateScope())
