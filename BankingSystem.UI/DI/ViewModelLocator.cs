da615212123d9e8da62ad505aa0fd37723b8e9a1
PTP <93674251+PianoTutorialsPlus@users.noreply.github.com>
2025-10-19 16:36:30 +0200

    Added DI

---
+++ BankingSystem.UI/DI/ViewModelLocator.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
+using BankingSystem.UI.Navigation;
+using BankingSystem.UI.Services;
+using BankingSystem.UI.Services.Base;
+using BankingSystem.UI.Services.Customer;
+using BankingSystem.UI.ViewModels;
+using BankingSystem.UI.ViewModels.Customers;
+using BankingSystem.UI.Views;
+using System.Reflection;
+
+namespace BankingSystem.UI.DI;
+
+public class ViewModelLocator
+{
+    public static IContainer Container { get; private set; }
+
+    static ViewModelLocator()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterAutoMapper(Assembly.GetExecutingAssembly());
+
+        // Services
+        builder.RegisterType<ApiService>().As<IApiService>().SingleInstance();
+        builder.RegisterType<CustomerService>().As<ICustomerService>().SingleInstance();
+
+        builder.RegisterType<Client>().As<IClient>().SingleInstance();
+        builder.RegisterType<NavigationService>().As<INavigationService>().SingleInstance();
+
+        // Views and ViewModels
+        builder.RegisterType<ShellMainView>().AsSelf().SingleInstance();
+
+        builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .Where(type => !type.IsAbstract && type.Name.EndsWith("ViewModel"))
+            .PropertiesAutowired()
+            .AsSelf()
+            .InstancePerDependency();
+
+        // RelayCommand
+        builder.RegisterType<RelayCommand>().AsSelf();
+
+        Container = builder.Build();
+    }
+
+    public ShellMainViewModel ShellMainViewModel => Container.Resolve<ShellMainViewModel>();
+}
