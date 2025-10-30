33341e42c995d2dda9b8a5aea5d0eb13dde4b68c
PTP <93674251+PianoTutorialsPlus@users.noreply.github.com>
2025-10-30 10:24:45 +0100

    Setting up E2E tests

---
+++ BankingSystem.E2ETests/TestServerFixture.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Playwright;
+
+namespace BankingSystem.E2ETests;
+
+[SetUpFixture]
+public class TestServerFixture
+{
+    public static IHost? ApiHost { get; private set; }
+    public static IHost? WebHost { get; private set; }
+    public static IPlaywright? PlaywrightInstance;
+    public static IBrowser? Browser;
+    public static string ApiUrl => ApiHost?.GetTestServer().BaseAddress.ToString() ?? "";
+    public static string WebUrl => WebHost?.GetTestServer().BaseAddress.ToString() ?? "";
+
+    [OneTimeSetUp]
+    public async Task GlobalSetup()
+    {
+        // Start the API in-memory
+        ApiHost = TestProgram.BuildHost();
+        await ApiHost.StartAsync();
+
+        WebHost = BuildBlazorHost();
+
+        // Initialize Playwright
+        PlaywrightInstance = await Microsoft.Playwright.Playwright.CreateAsync();
+
+        Browser = await PlaywrightInstance.Firefox.LaunchAsync(new()
+        {
+            Headless = false, // show browser
+            SlowMo = 100      // small delay for visual debugging
+        });
+    }
+
+    [OneTimeTearDown]
+    public async Task GlobalTeardown()
+    {
+        await Browser?.CloseAsync()!;
+        await ApiHost!.StopAsync();
+        await WebHost!.StopAsync();
+
+        WebHost?.Dispose();
+        ApiHost?.Dispose();
+        PlaywrightInstance?.Dispose();
+    }
+
+    public static IHost BuildBlazorHost()
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseTestServer();
+                webBuilder.Configure(app =>
+                {
+                    var webUiPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "BankingSystem.WebUI", "wwwroot");
+                    app.UseDefaultFiles();
+                    app.UseStaticFiles(new StaticFileOptions
+                    {
+                        FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(webUiPath)
+                    });
+                });
+            })
+            .Start();
+
+        return host;
+    }
+}
---
+++ BankingSystem.E2ETests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace BankingSystem.E2ETests
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        private IPage _page = null!;
+        private IBrowserContext _context = null!;
+        private string BaseUrl => TestServerFixture.WebUrl;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _context = await TestServerFixture.Browser!.NewContextAsync();
+            _page = await _context.NewPageAsync();
+        }
+
+        [TearDown]
+        public async Task Cleanup()
+        {
+            await _context.CloseAsync();
+        }
+
+        [Test]
+        public async Task Should_Login_Successfully()
+        {
+            await _page.GotoAsync($"{BaseUrl}login");
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+            await _page.WaitForSelectorAsync("#userName");
+            await _page.FillAsync("#userName", "user@localhost.com");
+            await _page.FillAsync("#password", "P@ssword1");
+
+            await _page.ClickAsync("input[value='Login']");
+            await _page.WaitForURLAsync("**/");
+
+            var heading = await _page.TextContentAsync("h1");
+            Assert.That(heading, Does.Contain("Welcome"));
+        }
+    }
+}
